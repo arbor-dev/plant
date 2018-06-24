95b2d04fa86d3c09abda53728dfe3c9fa68dea06
Rishi Desai <desai.rishi1@gmail.com>
2018-06-23 21:42:44 -0700

    updated license info

--- cmd/plant.go
+++ cmd/plant.go
@@ -1,9 +1,9 @@
 /**
 * Copyright © 2018, ACM@UIUC
 *
-* This file is part of the Groot Project.
+* This file is part of the Arbor Project.
 *
-* The Groot Project is open source software, released under the University of
+* The Arbor Project is open source software, released under the University of
 * Illinois/NCSA Open Source License. You should have received a copy of
 * this license in a file with the distribution.
 **/
